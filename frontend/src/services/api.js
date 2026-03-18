7dfc06e043b7ed324a7ddfd91a9762ecb149e335
SOKYANG.LAKK <SOKYANG.LAKK@PNC.LOCAL>
2026-03-18 08:02:11 +0700

    fix: define baseURL in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
 import { startApiLoading, stopApiLoading } from './loadingService'
 
 const api = axios.create({
